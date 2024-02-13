4858336c90c0e5619ceb18c8b7564f88cd6e3931
Himprakash Deka <himprakashd@gmail.com>
2024-02-13 20:25:39 +0530

    update

--- scripts/repo.js
+++ scripts/repo.js
@@ -62,6 +62,44 @@ fetch("/repopagetemplate.html").then(function (response) {
                         el.innerHTML = timeAgo(new Date(out.pushed_at))
                     });
 
+                    svg = "<svg height=\"26\" aria-hidden=\"true\" viewBox=\"0 0 16 16\" version=\"1.1\" width=\"26\" data-view-component=\"true\"><path d=\"M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z\"></path></svg>"
+
+                    value = document.createElement("div")
+                    value.innerHTML = "GitHub Repository"
+
+                    stat = document.createElement("div")
+                    stat.classList.add("fs-6")
+                    stat.classList.add("text-body-secondary")
+                    stat.innerHTML = "Source Code"
+
+                    body = document.createElement("span")
+                    body.appendChild(value)
+                    body.appendChild(stat)
+
+                    icon = document.createElement("span")
+                    icon.classList.add("pe-4")
+                    icon.innerHTML = svg
+
+                    span = document.createElement("span")
+                    span.classList.add("d-flex")
+                    span.classList.add("align-items-center")
+                    span.classList.add("justify-items-start")
+                    span.classList.add("text-body")
+                    span.classList.add("ff-mono")
+                    span.appendChild(icon)
+                    span.appendChild(body)
+
+                    a = document.createElement("a")
+                    a.setAttribute('href', out.html_url)
+                    a.appendChild(span)
+
+                    li = document.createElement("li")
+                    li.classList.add("list-group-item")
+                    li.classList.add("bg-transparent")
+                    li.appendChild(a)
+
+                    stats.appendChild(li)
+
                     if (out.language != null) {
                         svg = "<svg style=\"padding: 0px; margin:0;\" height=\"26\" width=\"26\"><circle id=\"" + out.id + "\" cx=\"13\" cy=\"13\" r=\"12\" stroke=\"white\" stroke-width=\"1\"></circle></svg>"
 
