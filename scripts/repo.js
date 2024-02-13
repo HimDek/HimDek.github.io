7c68e6316dfe50a193f30a8361acfc10a791ac23
Himprakash Deka <himprakashd@gmail.com>
2024-02-13 18:41:26 +0530

    update

--- scripts/repo.js
+++ scripts/repo.js
@@ -186,7 +186,7 @@ fetch("/repopagetemplate.html").then(function (response) {
                         svg = "<svg aria-hidden=\"true\" height=\"26\" viewBox=\"0 0 16 16\" version=\"1.1\" width=\"26\" data-view-component=\"true\"><path d=\"M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z\"></path><path d=\"M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Z\"></path></svg>"
 
                         value = document.createElement("div")
-                        value.innerHTML = out.forks.toString()
+                        value.innerHTML = out.open_issues
 
                         stat = document.createElement("div")
                         stat.classList.add("fs-6")
