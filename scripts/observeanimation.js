8400820676d67d78cae6f4581da1bc4c938d9202
Himprakash Deka <himprakashd@gmail.com>
2024-02-14 14:12:27 +0530

    update

--- scripts/observeanimation.js
+++ scripts/observeanimation.js
@@ -1,24 +1,17 @@
 function observeanimation() {
-    const typing = document.querySelectorAll('.typing');
-    typing.forEach(el => typingobserver.observe(el));
     document.getElementById('theme-icon').classList.add('shrunk');
+    document.querySelectorAll('.typing').forEach(el => typingobserver.observe(el));
     document.querySelectorAll('.anb').forEach(element => element.classList.add('shrunk'));
     document.querySelectorAll('.badge').forEach(element => element.classList.add('shrunk'));
     document.querySelectorAll('.tabmenu, .activetab, .tablinks').forEach(element => element.classList.add('hiddenLeft'));
-    const hiddenUp = document.querySelectorAll('.hiddenUp');
-    hiddenUp.forEach(el => observer.observe(el));
-    const hiddenDown = document.querySelectorAll('.hiddenDown');
-    hiddenDown.forEach(el => observer.observe(el));
-    const hiddenLeft = document.querySelectorAll('.hiddenLeft');
-    hiddenLeft.forEach(el => observer.observe(el));
-    const hiddenRight = document.querySelectorAll('.hiddenRight');
-    hiddenRight.forEach(el => observer.observe(el));
-    const hiddenX = document.querySelectorAll('.hiddenX');
-    hiddenX.forEach(el => observer.observe(el));
-    const hiddenY = document.querySelectorAll('.hiddenY');
-    hiddenY.forEach(el => observer.observe(el));
-    const shrunk = document.querySelectorAll('.shrunk');
-    shrunk.forEach(el => observer.observe(el));
+    document.querySelectorAll('.hiddenUp').forEach(el => observer.observe(el));
+    document.querySelectorAll('.hiddenDown').forEach(el => observer.observe(el));
+    document.querySelectorAll('.hiddenLeft').forEach(el => observer.observe(el));
+    document.querySelectorAll('.hiddenRight').forEach(el => observer.observe(el));
+    document.querySelectorAll('.hiddenX').forEach(el => observer.observe(el));
+    document.querySelectorAll('.hiddenY').forEach(el => observer.observe(el));
+    document.querySelectorAll('.shrunk').forEach(el => observer.observe(el));
+    document.querySelectorAll('.bg-contain').forEach(el => observer.observe(el));
 }
 
 const typingobserver = new IntersectionObserver(entries => {
@@ -37,10 +30,12 @@ const observer = new IntersectionObserver(entries => {
         if (entry.isIntersecting) {
             entry.target.classList.add('shown');
             entry.target.classList.add('expanded');
+            entry.target.classList.add('bg-show');
         }
         else {
             entry.target.classList.remove('shown');
             entry.target.classList.remove('expanded');
+            entry.target.classList.remove('bg-show');
         }
     });
 });
