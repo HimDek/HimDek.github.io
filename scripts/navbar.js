973cd565c246cc2ec8fb3c09b48b279ad917f758
Himprakash Deka <himprakashd@gmail.com>
2024-02-22 17:17:28 +0530

    update

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,14 +1,14 @@
 function Menu(icon) {
     if (document.getElementById("nav").classList.contains("responsive")) {
         document.getElementById("nav").classList.remove("responsive");
-        document.getElementById("navmenu-hamicon").classList.remove("responsive")
+        document.getElementById("nav-menu-hamicon").classList.remove("responsive")
         document.getElementById("navbar").classList.remove("bg-primary-subtle")
         document.getElementById("navbar").classList.add("bg-accent-subtle")
         document.getElementById("nav").classList.remove("bg-primary-subtle")
         document.getElementById("nav").classList.add("bg-accent-subtle")
     } else {
         document.getElementById("nav").classList.add("responsive");
-        document.getElementById("navmenu-hamicon").classList.add("responsive");
+        document.getElementById("nav-menu-hamicon").classList.add("responsive");
         document.getElementById("navbar").classList.remove("bg-accent-subtle")
         document.getElementById("navbar").classList.add("bg-primary-subtle")
         document.getElementById("nav").classList.remove("bg-accent-subtle")
